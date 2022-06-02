5e73771d3c1230a537b095accc85edc4a6c2fd89
vpanal <106394237+vpanal@users.noreply.github.com>
2022-06-02 19:52:42 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -2,15 +2,16 @@
 docker build -t custom .
 docker-compose up -d
 domain=$1
+file=$domain.yml
 
-echo 'site-'$domain':'>>$domain
-echo '  image:  custom'>>$domain
-echo '  restart: always'>>$domain
-echo '  expose:'>>$domain
-echo '    - "80"'>>$domain
-echo '    - "8080"'>>$domain
-echo '  volumes:'>>$domain
-echo '    - /etc/dockup/mountpoint:/var/www/html:ro'>>$domain
-echo '  environment:'>>$domain
-echo '    - VIRTUAL_HOST='$domain>>$domain
-docker-compose -f $domain.yml up -d
+echo 'site-'$domain':'>>$file
+echo '  image:  custom'>>$file
+echo '  restart: always'>>$file
+echo '  expose:'>>$file
+echo '    - "80"'>>$file
+echo '    - "8080"'>>$file
+echo '  volumes:'>>$file
+echo '    - /etc/dockup/mountpoint:/var/www/html:ro'>>$file
+echo '  environment:'>>$file
+echo '    - VIRTUAL_HOST='$domain>>$file
+docker-compose -f $file.yml up -d
