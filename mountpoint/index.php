ae866955c6609819d5856e329e96db838290235a
vpanal <106394237+vpanal@users.noreply.github.com>
2022-06-08 09:56:28 +0200

    Update index.php

--- mountpoint/index.php
+++ mountpoint/index.php
@@ -9,7 +9,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Home</title>
+    <title>Dockup Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Dependencias -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
